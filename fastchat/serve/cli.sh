3048ad34cae07ee74b0bf4b0e975a94f01c58ad1
minhvn <minhvn@rikkeisoft.com>
2023-08-03 07:24:33 +0000

    update model-path for Peft Lora vicuna-1.5

--- fastchat/serve/cli.sh
+++ fastchat/serve/cli.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 \
+CUDA_VISIBLE_DEVICES=1 \
 python3 -m fastchat.serve.cli \
-    --model peft_lora_model \
-    --style rich \
+    --model peft_lora_model_vicuna1.5_7b_15epochs \
     --no-history \
     --debug \
+    --style rich \
 
--- fastchat/serve/model_worker.sh
+++ fastchat/serve/model_worker.sh
@@ -1,2 +1,2 @@
 python3 -m fastchat.serve.model_worker \
-         --model-path peft_lora_model \
+         --model-path peft_lora_model_vicuna1.5_7b_15epochs \
