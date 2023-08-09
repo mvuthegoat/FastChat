3bd65c351f5bc558e0bcdbbd8f247820b38094d7
minhvn <minhvn@rikkeisoft.com>
2023-08-09 07:59:06 +0000

    update model_worker.sh script

--- fastchat/serve/model_worker.sh
+++ fastchat/serve/model_worker.sh
@@ -1,2 +1,4 @@
-python3 -m fastchat.serve.model_worker \
-         --model-path peft_lora_model_vicuna1.5_7b_15epochs \
+# worker 0
+CUDA_VISIBLE_DEVICES=0 python3 -m fastchat.serve.model_worker --model-path peft_lora_model_vicuna1.5_7b_15epochs --port 31000 --worker http://localhost:31000
+# worker 1
+CUDA_VISIBLE_DEVICES=1 python3 -m fastchat.serve.model_worker --model-path peft_lora_model_vicuna1.3_7b_20epochs --port 31001 --worker http://localhost:31001
